0812576b86b6e834f46c390d04d5fbd3ccbb2b18
Taeyoon Lee <taeyoon0137@gmail.com>
2022-12-26 15:46:23 +0900

    update: 기본 페이지 설정

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,22 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+/*
+  Copyright 2022. Taeyoon Lee.
+  All rights reserved
+*/
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
-}
+import '../styles/initialize.css';
+
+import type { AppProps } from 'next/app';
+
+/**
+ * ### 앱
+ *
+ * NEXT.js에서 전체 앱을 래핑하는 루트 컴포넌트입니다.
+ *
+ * @param props AppProps
+ * @component
+ */
+const App = ({ Component, pageProps }: AppProps) => {
+  return <Component {...pageProps} />;
+};
+
+export default App;
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,13 +1,29 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+/*
+  Copyright 2022. Taeyoon Lee.
+  All rights reserved
+*/
 
-export default function Document() {
+import { Head, Html, Main, NextScript } from 'next/document';
+
+/**
+ * ### 도큐먼트
+ *
+ * NEXT.js에서 각 페이지를 래핑하는 루트 컴포넌트입니다.
+ *
+ * @component
+ */
+const Document = () => {
   return (
-    <Html lang="en">
-      <Head />
+    <Html lang='en'>
+      <Head>
+        <meta name='viewport' content='width=device-width, initial-scale=1' />
+      </Head>
       <body>
         <Main />
         <NextScript />
       </body>
     </Html>
-  )
-}
+  );
+};
+
+export default Document;
