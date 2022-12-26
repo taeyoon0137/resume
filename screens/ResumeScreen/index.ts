24cd6670e3fce351b794650a77ec332c84d588ef
Taeyoon Lee <taeyoon0137@gmail.com>
2022-12-26 18:01:23 +0900

    update: 페이지 컴포넌트 분리

---
+++ screens/ResumeScreen/index.ts
@@ -0,0 +1,6 @@
+/*
+  Copyright 2022. Taeyoon Lee.
+  All rights reserved
+*/
+
+export { default as ResumeScreen } from './ResumeScreen';
---
+++ screens/index.ts
@@ -0,0 +1,6 @@
+/*
+  Copyright 2022. Taeyoon Lee.
+  All rights reserved
+*/
+
+export * from './ResumeScreen';
