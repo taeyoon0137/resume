6365d037722795cbc30d2f02d637b85ba7c94854
taeyoon0137 <taeyoon0137@gmail.com>
2024-08-18 16:42:48 +0900

    Feat: favicon 설정

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -8,7 +8,7 @@
 import "../styles/css/globals.css";
 
 import type { LayoutProps } from "@/types";
-import type { Metadata } from "next";
+import type { Metadata, Viewport } from "next";
 
 /**
  * ### metadata 프로퍼티
@@ -18,6 +18,36 @@ import type { Metadata } from "next";
 export const metadata: Metadata = {
   title: "taeyoon. – resume",
   description: "반갑습니다. Product Designer & FE Engineer(RN)로 활동 중인 이태윤입니다.",
+  icons: [
+    { rel: "apple-touch-icon", sizes: "57x57", url: "/favicon/apple-icon-57x57.png" },
+    { rel: "apple-touch-icon", sizes: "60x60", url: "/favicon/apple-icon-60x60.png" },
+    { rel: "apple-touch-icon", sizes: "72x72", url: "/favicon/apple-icon-72x72.png" },
+    { rel: "apple-touch-icon", sizes: "76x76", url: "/favicon/apple-icon-76x76.png" },
+    { rel: "apple-touch-icon", sizes: "114x114", url: "/favicon/apple-icon-114x114.png" },
+    { rel: "apple-touch-icon", sizes: "120x120", url: "/favicon/apple-icon-120x120.png" },
+    { rel: "apple-touch-icon", sizes: "144x144", url: "/favicon/apple-icon-144x144.png" },
+    { rel: "apple-touch-icon", sizes: "152x152", url: "/favicon/apple-icon-152x152.png" },
+    { rel: "apple-touch-icon", sizes: "180x180", url: "/favicon/apple-icon-180x180.png" },
+    { rel: "icon", type: "image/png", sizes: "192x192", url: "/favicon/android-icon-192x192.png" },
+    { rel: "icon", type: "image/png", sizes: "32x32", url: "/favicon/favicon-32x32.png" },
+    { rel: "icon", type: "image/png", sizes: "96x96", url: "/favicon/favicon-96x96.png" },
+    { rel: "icon", type: "image/png", sizes: "16x16", url: "/favicon/favicon-16x16.png" },
+    { rel: "icon", type: "image/png", sizes: "36x36", url: "/favicon/android-icon-36x36.png" },
+    { rel: "icon", type: "image/png", sizes: "48x48", url: "/favicon/android-icon-48x48.png" },
+    { rel: "icon", type: "image/png", sizes: "72x72", url: "/favicon/android-icon-72x72.png" },
+    { rel: "icon", type: "image/png", sizes: "96x96", url: "/favicon/android-icon-96x96.png" },
+    { rel: "icon", type: "image/png", sizes: "144x144", url: "/favicon/android-icon-144x144.png" },
+    { rel: "icon", type: "image/png", sizes: "192x192", url: "/favicon/android-icon-192x192.png" },
+  ],
+};
+
+/**
+ * ### viewport 프로퍼티
+ *
+ * 페이지의 뷰포트를 정의합니다.
+ */
+export const viewport: Viewport = {
+  themeColor: "#000000",
 };
 
 /**
