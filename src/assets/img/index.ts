3a70b52ec2c9dc910ddf72faa4b56b6311cbfe69
Taeyoon Lee <taeyoon0137@gmail.com>
2026-06-10 23:54:08 +0900

    Feat: 프로젝트 콘텐츠 업데이트

--- src/assets/img/index.ts
+++ src/assets/img/index.ts
@@ -6,18 +6,27 @@
  */
 
 // Project Thumbnail
-export { default as img_project_thumbnail_whatssub_blog } from "./img_project_thumbnail_whatssub_blog.png";
-export { default as img_project_thumbnail_whatssub } from "./img_project_thumbnail_whatssub.png";
-export { default as img_project_thumbnail_whatssub_global } from "./img_project_thumbnail_whatssub_global.png";
-export { default as img_project_thumbnail_whatssub_figma_plugin } from "./img_project_thumbnail_whatssub_figma_plugin.png";
+export { default as img_activity_thumbnail_design_off } from "./img_activity_thumbnail_design_off.png";
+export { default as img_activity_thumbnail_medium_semantic } from "./img_activity_thumbnail_medium_semantic.png";
+export { default as img_activity_thumbnail_whatssub_blog_interview } from "./img_activity_thumbnail_whatssub_blog_interview.png";
 export { default as img_project_thumbnail_bang } from "./img_project_thumbnail_bang.png";
+export { default as img_project_thumbnail_vendit_at_gangnam } from "./img_project_thumbnail_vendit_at_gangnam.png";
+export { default as img_project_thumbnail_vendit_brochure } from "./img_project_thumbnail_vendit_brochure.png";
+export { default as img_project_thumbnail_vendit_icon } from "./img_project_thumbnail_vendit_icon.png";
+export { default as img_project_thumbnail_vendit_ir } from "./img_project_thumbnail_vendit_ir.png";
+export { default as img_project_thumbnail_vendit_landing } from "./img_project_thumbnail_vendit_landing.png";
+export { default as img_project_thumbnail_vendit_vds } from "./img_project_thumbnail_vendit_vds.png";
+export { default as img_project_thumbnail_vendit_vkiosk } from "./img_project_thumbnail_vendit_vkiosk.png";
+export { default as img_project_thumbnail_vendit_vpms } from "./img_project_thumbnail_vendit_vpms.png";
+export { default as img_project_thumbnail_whatssub } from "./img_project_thumbnail_whatssub.png";
+export { default as img_project_thumbnail_whatssub_blog } from "./img_project_thumbnail_whatssub_blog.png";
 export { default as img_project_thumbnail_whatssub_design_system } from "./img_project_thumbnail_whatssub_design_system.png";
+export { default as img_project_thumbnail_whatssub_figma_plugin } from "./img_project_thumbnail_whatssub_figma_plugin.png";
+export { default as img_project_thumbnail_whatssub_global } from "./img_project_thumbnail_whatssub_global.png";
+
 export { default as img_side_project_thumbnail_cc } from "./img_side_project_thumbnail_cc.png";
 export { default as img_side_project_thumbnail_echoscript } from "./img_side_project_thumbnail_echoscript.png";
 export { default as img_side_project_thumbnail_sdvd } from "./img_side_project_thumbnail_sdvd.png";
-export { default as img_activity_thumbnail_design_off } from "./img_activity_thumbnail_design_off.png";
-export { default as img_activity_thumbnail_whatssub_blog_interview } from "./img_activity_thumbnail_whatssub_blog_interview.png";
-export { default as img_activity_thumbnail_medium_semantic } from "./img_activity_thumbnail_medium_semantic.png";
 
 // Symbol
 export { default as TaeyoonSymbol } from "./taeyoon_symbol.svg";
